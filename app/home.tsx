84891b98bd2042bbfbeac0f37a96d57284567b50
Kartik Gothwal <kartikgothwal28@gmail.com>
2024-11-07 16:44:53 +0530

    changes the component

--- app/home.tsx
+++ app/home.tsx
@@ -2,13 +2,9 @@
 import Image from "next/image";
 import { homepage, Logo2 } from "@/public";
 import { NavigationMenuDemo } from "@/components/lib";
-import { Dialog, DialogTrigger } from "@/components/ui/dialog";
-import { Button } from "@/components/lib/button";
-import GettingStarted from "@/app/_getting-started/page";
-import { useState } from "react";
+import LoginSignupModel from "@/components/models/login-signup-model";
 
 const Home = (): JSX.Element => {
-    const [getStarted, setGetStarted] = useState<boolean>(false)
     return <>
         <section className="h-screen ">
             <div className="flex justify-between mx-12 my-4">
@@ -18,7 +14,6 @@ const Home = (): JSX.Element => {
                         alt="logo"
                         width={100}
                         height={100}
-
                     />
                 </div>
                 <div className="mr-[10rem]">
@@ -36,16 +31,7 @@ const Home = (): JSX.Element => {
                         </h1>
                     </h1>
                     <p className="leading-[22px] text-[22px]">A place to read, write, and deepen your understanding</p>
-                    <Dialog open={getStarted} onOpenChange={setGetStarted}>
-                        <DialogTrigger asChild>
-                            <Button classname={"bg-black text-white w-[10rem] p-[1rem] rounded-[1rem]"}
-                                variant="outline" onclick={() => {
-                                    setGetStarted(!getStarted)
-                                }}>Edit Profile</Button>
-                        </DialogTrigger>
-                        <GettingStarted />
-                    </Dialog>
-
+                    <LoginSignupModel />
                 </div>
                 <div>
                     <Image
