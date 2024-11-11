b93ebb78b2a57cb018ec99766bf1f3577e34c5ea
Kartik Gothwal <127651456+kartikgothwal@users.noreply.github.com>
2024-11-11 08:22:22 +0000

    some changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,9 @@ import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import client from "../../../../lib/db";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-const handler = NextAuth({
+import axios, { AxiosResponse } from "axios";
+import { USER_LOGIN } from "@/constant";
+const handler: any = NextAuth({
   adapter: MongoDBAdapter(client),
   providers: [
     GoogleProvider({
@@ -15,5 +17,26 @@ const handler = NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }): Promise<boolean> {
+      try {
+        const response: AxiosResponse<any> = await axios.post(USER_LOGIN, {
+          email: user.email,
+          password: 123456,
+        });
+        console.log(">>>>", response);
+        return true;
+      } catch (error) {
+        console.error("Error in signIn callback:", error);
+        return false;
+      }
+    },
+
+    //  async session({ session, token, user }) {
+    //   // Attach any additional data to the session here
+    //   session.user.customData = user.customData || null; // Pass custom data to the session
+    //   return session;
+    // },
+  },
 });
 export { handler as GET, handler as POST };
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
 import Home from "@/app/home";
-import { DbConnect } from "@/config/dbConnect";
 
 const Page = (): JSX.Element => {
-  async function db() {
-    await DbConnect()
-  }
-  db()
   return (
     <>
       <Home />
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
