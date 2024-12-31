9d070cf24f3cc95189788ca9031f66d6d0a46ecc
Kartik Gothwal <kartikgothwal28@gmail.com>
2024-12-31 22:47:14 +0530

    Added pnpm

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-
+ 
 const page = () => {
   return (
     <>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore rem ipsam omnis modi earum sapiente officiis eligendi error, nostrum quia dolor, aperiam similique ullam dicta, ut magni maxime vero vel.</p>
+      <p>
+       Lavda
+      </p>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
