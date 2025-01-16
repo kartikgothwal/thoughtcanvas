bf3465578cf23ac73dbba031daa35f73da1451df
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-01-17 01:24:22 +0530

    added the signup form

--- components/layout/Hero.tsx
+++ components/layout/Hero.tsx
@@ -1,10 +1,14 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { buttonVariants } from "@/components/ui/button";
 import { HeroCards } from "./HeroCards";
 // import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 import { styles } from "@/utils/styles";
+import { useState } from "react";
+import { SignUpForm } from "./Signup";
 export const Hero = () => {
+  const [openSignUpModel, setOpenSignupModal] = useState<boolean>(false);
   return (
     <section
       className={`container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10 ${styles.paddingX} `}
@@ -14,19 +18,24 @@ export const Hero = () => {
           <h1 className="inline">
             <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
               Human
-              
             </span>{" "}
             stories & ideas
           </h1>{" "}
         </main>
 
         <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
-        A place to read, write, and deepen your understanding
-
+          A place to read, write, and deepen your understanding
         </p>
 
         <div className="space-y-4 md:space-y-0 md:space-x-4">
-          <Button className="w-full md:w-1/3">Get Started</Button>
+          <Button
+            className="w-full md:w-1/3"
+            onClick={() => {
+              setOpenSignupModal(!openSignUpModel);
+            }}
+          >
+            Get Started
+          </Button>
 
           <Link
             rel="noreferrer noopener"
@@ -49,6 +58,7 @@ export const Hero = () => {
 
       {/* Shadow effect */}
       <div className="shadow"></div>
+      {openSignUpModel && <SignUpForm openSignUpModel={openSignUpModel}/>}
     </section>
   );
 };
