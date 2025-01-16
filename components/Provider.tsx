6ee410de2c84aad75abbf868a3b8a8c4962de256
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-01-16 10:41:46 +0530

    added a provider and shadcn

---
+++ components/Provider.tsx
@@ -0,0 +1,23 @@
+import { ThemeProvider } from "@/components/theme-provider";
+
+const Provider = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <>
+      {" "}
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default Provider;
