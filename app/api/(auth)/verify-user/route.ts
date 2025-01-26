b8824b7ae5e181186cb34bff6e5de8c199043caa
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-01-26 20:11:59 +0530

    added the key

--- app/api/(auth)/verify-user/route.ts
+++ app/api/(auth)/verify-user/route.ts
@@ -1,16 +1,52 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
-export async function POST(request: NextRequest) {
+export async function POST(request: NextRequest): Promise<unknown> {
   try {
     const headers = request.headers;
-    const authorization = headers.get("Authorization")?.split(" ")[1];
-    if (!authorization) {
-        
+    const authorizationToken: string | undefined = headers
+      .get("Authorization")
+      ?.split(" ")[1];
+    if (!authorizationToken) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
     if (!process.env.NEXT_JWT_PUBLIC_KEY) {
       throw new Error("Missing public key in environment variables.");
     }
-    return jwt.verify("token", process.env.NEXT_JWT_PUBLIC_KEY);
+    const decodedHeader: any = jwt.decode(authorizationToken, {
+      complete: true,
+    });
+    console.log("🚀 ~ Token Header:", decodedHeader);
+
+    if (!decodedHeader) {
+      return NextResponse.json({ message: "Invalid Token" }, { status: 401 });
+    }
+
+    const algorithm = decodedHeader.header.alg;
+    console.log("🚀 ~ POST ~ algorithm:", algorithm);
+
+    // Handle based on algorithm
+    let isValidToken;
+    if (algorithm === "RS256") {
+      isValidToken = jwt.verify(
+        authorizationToken,
+        process.env.NEXT_JWT_PUBLIC_KEY,
+        {
+          algorithms: ["RS256"],
+        }
+      );
+    } else if (algorithm === "HS256") {
+      if (!process.env.NEXT_JWT_PUBLIC_KEY) {
+        throw new Error("Missing secret key in environment variables.");
+      }
+      isValidToken = jwt.verify(
+        authorizationToken,
+        process.env.NEXT_JWT_PUBLIC_KEY,
+        {
+          algorithms: ["HS256"],
+        }
+      );
+    }
+    console.log("🚀 ~ POST ~ isValidToken:", isValidToken);
   } catch (error: unknown) {
     console.log("🚀 ~ VerifyJwtToken ~ error:", error);
   }
