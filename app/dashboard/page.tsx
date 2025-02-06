037774c11c9704275bcbabec8e65d9c1c923d772
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-02-06 23:23:11 +0530

    added the added the one main hero section

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -68,7 +68,7 @@ export default function SidebarDemo() {
                 href: "#",
                 icon: (
                   <Image
-                    src=""
+                    src="/"
                     className="h-7 w-7 flex-shrink-0 rounded-full"
                     width={50}
                     height={50}
@@ -89,7 +89,7 @@ export const Logo = () => {
     <div className="flex flex-row justify-between">
       <Link
         href="#"
-        className="border border-black font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+        className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
       >
         <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
 
@@ -120,21 +120,11 @@ const Dashboard = () => {
   return (
     <div className="flex flex-1">
       <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
-        <div className="flex gap-2">
-          {[...new Array(4)].map((i) => (
-            <div
-              key={"first-array" + i}
-              className="h-20 w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
-            ></div>
-          ))}
-        </div>
         <div className="flex gap-2 flex-1">
-          {[...new Array(2)].map((i) => (
-            <div
-              key={"second-array" + i}
-              className="h-full w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
-            ></div>
-          ))}
+          <div className="h-full w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+            minus?
+          </div>
         </div>
       </div>
     </div>
