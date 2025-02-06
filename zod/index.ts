cc57b2f6369d65fec38552c3379a6fdba0fe4390
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-02-06 21:50:59 +0530

    removed the data

--- zod/index.ts
+++ zod/index.ts
@@ -27,30 +27,4 @@ export const SignUpFormSchema = z.object({
       "must be 8-12 characters long, include at least one uppercase letter, one lowercase letter, one number, and one special character"
     ),
 });
-export const SignUpFormSchema2 = z.object({
-  firstname: z
-    .string()
-    .min(3, "Firstname must greater than 3 characters")
-    .max(25, "Firstname must less than 25 characters")
-    .regex(
-      /^[a-zA-Z0-9_]+$/,
-      "must contain only letters, numbers and underscore (_)"
-    ),
-  lastname: z
-    .string()
-    .min(3, "Lastname must greater than 3 characters")
-    .max(25, "Lastname must less than 25 characters")
-    .regex(
-      /^[a-zA-Z0-9_]+$/,
-      "must contain only letters, numbers and underscore (_)"
-    ),
-  email: z.string().email("Email must be a valid email address"),
-  password: z
-    .string()
-    .min(8, "Password must greater than 8 characters")
-    .max(12, "Password must less than 12 characters")
-    .regex(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[a-zA-Z\d@$!%*?&]{8,12}$/,
-      "must be 8-12 characters long, include at least one uppercase letter, one lowercase letter, one number, and one special character"
-    ),
-});
+
