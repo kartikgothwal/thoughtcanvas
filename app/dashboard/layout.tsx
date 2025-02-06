75266682cbf52a64454f7c23cf57460936cb5fc2
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-02-06 22:02:56 +0530

    added the proper sidebar open

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,6 @@
-// import { SidebarDemo } from "@/app/dashboard/page";
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {/* <SidebarDemo /> */}
       <main>{children}</main>
     </>
   );
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,10 +11,8 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
-import { JSX } from "react";
-import { ModeToggle } from "@/components/ui/mode-toggle";
 
-export default function SidebarDemo(): JSX.Element {
+export default function SidebarDemo() {
   const links = [
     {
       label: "Dashboard",
@@ -49,16 +47,13 @@ export default function SidebarDemo(): JSX.Element {
   return (
     <div
       className={cn(
-        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1  mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
-        "h-screen" // for your use case, use `h-screen` instead of `h-[60vh]`
+        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 flex-1  max-w-full mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden h-screen"
       )}
     >
-      <Sidebar open={open} setOpen={setOpen} animate={false}>
+      <Sidebar open={open} setOpen={setOpen}>
         <SidebarBody className="justify-between gap-10">
           <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-            <>
-              <Logo />
-            </>
+            {open ? <Logo /> : <LogoIcon />}
             <div className="mt-8 flex flex-col gap-2">
               {links.map((link, idx) => (
                 <SidebarLink key={idx} link={link} />
@@ -68,7 +63,7 @@ export default function SidebarDemo(): JSX.Element {
           <div>
             <SidebarLink
               link={{
-                label: "Kartik Gothwal",
+                label: "Manu Arora",
                 href: "#",
                 icon: (
                   <Image
@@ -88,29 +83,24 @@ export default function SidebarDemo(): JSX.Element {
     </div>
   );
 }
-export const Logo = (): JSX.Element => {
+export const Logo = () => {
   return (
-    <>
-      <div className="flex items-center justify-between  py-4">
-        <Link
-          href="#"
-          className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
-        >
-          <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
-          <motion.span
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            className="font-medium text-black dark:text-white whitespace-pre"
-          >
-            ThoughtCanvas
-          </motion.span>
-        </Link>
-        <ModeToggle />
-      </div>
-    </>
+    <Link
+      href="#"
+      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+    >
+      <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className="font-medium text-black dark:text-white whitespace-pre"
+      >
+        Acet Labs
+      </motion.span>
+    </Link>
   );
 };
-export const LogoIcon = (): JSX.Element => {
+export const LogoIcon = () => {
   return (
     <Link
       href="#"
@@ -122,14 +112,14 @@ export const LogoIcon = (): JSX.Element => {
 };
 
 // Dummy dashboard component with content
-const Dashboard = (): JSX.Element => {
+const Dashboard = () => {
   return (
     <div className="flex flex-1">
       <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
         <div className="flex gap-2">
           {[...new Array(4)].map((i) => (
             <div
-              key={"first" + i}
+              key={"first-array" + i}
               className="h-20 w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
             ></div>
           ))}
@@ -137,7 +127,7 @@ const Dashboard = (): JSX.Element => {
         <div className="flex gap-2 flex-1">
           {[...new Array(2)].map((i) => (
             <div
-              key={"second" + i}
+              key={"second-array" + i}
               className="h-full w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
             ></div>
           ))}
