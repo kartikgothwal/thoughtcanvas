089f5b60da15a6134c6782301222848a6ab9c70c
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-02-14 00:30:07 +0530

    added the create post and search button

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -120,7 +120,7 @@ export const LogoIcon = () => {
 const Dashboard = () => {
   return (
     <div className="flex flex-1 ">
-      <div className="p-2 md:p-6 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+      <div className="p-2 md:p-5 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-4 flex-1 w-full h-full">
         <div className="flex justify-between items-center">
           <div className="w-1/4">
             <Input type="text" placeholder="Search Blogs and Peoples" />
