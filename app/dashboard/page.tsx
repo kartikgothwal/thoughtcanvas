0e2e390f5418b1c6e3df9b5b1408f01ee3ba1b70
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-02-13 23:58:22 +0530

    added the padding

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -118,7 +118,7 @@ export const LogoIcon = () => {
 const Dashboard = () => {
   return (
     <div className="flex flex-1">
-      <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+      <div className="p-2 md:p-6 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
         <div className="flex gap-2 flex-1">
           <div className="h-full w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 ">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
