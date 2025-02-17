6d942dc7bba3319ae14377b622cc84bc0938713c
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-02-17 23:54:23 +0530

    added the loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import { About } from "@/components/layout/About";
 import { Hero } from "@/components/layout/Hero";
 import { Navbar } from "@/components/layout/Navbar";
-import { Loading } from "@/utils/LoadingUI";
+import LoadingSpinner from "@/utils/LoadingUI";
 const Page = () => {
   return (
     <div className="flex justify-center items-center">
       {/* <Navbar />
       <Hero />
       <About /> */}
-      <Loading />
+      <LoadingSpinner />
     </div>
   );
 };
--- utils/LoadingUI.tsx
+++ utils/LoadingUI.tsx
@@ -1,9 +1,12 @@
-export const Loading = () => {
+const LoadingSpinner = () => {
   return (
-    <>
-      <div className="flex-col gap-4 w-full flex items-center justify-center">
-        <div className="w-20 h-20 border-8 text-blue-400 text-4xl animate-spin border-gray-300 flex items-center justify-center border-t-blue-400 rounded-full"></div>
-      </div>
-    </>
+    <div
+      className="flex items-center justify-center h-screen w-full"
+      style={{ backgroundColor: "rgba(192, 192, 192, 0.5)" }}
+    >
+      <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32"></div>
+    </div>
   );
 };
+
+export default LoadingSpinner;
