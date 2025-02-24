2bb9c6cee886799866efc651366cdea246b2eb99
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-02-24 18:48:55 +0530

    added the status codes

--- constant/index.ts
+++ constant/index.ts
@@ -18,3 +18,16 @@ export const stats: statsProps[] = [
       description: "Products",
     },
   ];
+
+  export const ERROR_400 = 400; // Bad Request
+  export const ERROR_401 = 401; // Unauthorized
+  export const ERROR_403 = 403; // Forbidden
+  export const ERROR_404 = 404; // Not Found
+  export const ERROR_500 = 500; // Internal Server Error
+  export const ERROR_501 = 501; // Not Implemented
+  export const ERROR_502 = 502; // Bad Gateway
+  
+  export const SUCCESS_CODE_200 = 200; // OK
+  export const SUCCESS_CODE_201 = 201; // Created
+  export const SUCCESS_CODE_202 = 202; // Accepted
+  
