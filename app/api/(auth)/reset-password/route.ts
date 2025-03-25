ca513c91e5386eb0594a792af4f1accc7cb2e3db
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-03-26 00:49:50 +0530

    added the proper formatting

--- app/api/(auth)/reset-password/route.ts
+++ app/api/(auth)/reset-password/route.ts
@@ -11,7 +11,7 @@ async function handler(request: NextApiRequest) {
   try {
     const payload: ResetPasswordType = request.body;
     const isValidPayload = ResetPasswordSchema.safeParse(payload);
-    return NextResponse.json({ message: "Reset Password" });
+    return NextResponse.json({ message: "Reset Password",isValidPayload, payload });
   } catch (error) {
     console.error("🚀 ~ PATCH ~ error:", error);
     return handleError(error, "Internal Server Error");
