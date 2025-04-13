1441ff35d85a571642d0df232e11029ebcced936
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-04-13 23:41:20 +0530

    added the type for the component

--- components/layout/Statistics.tsx
+++ components/layout/Statistics.tsx
@@ -1,15 +1,13 @@
 import { stats } from "@/constant";
 import { statsProps } from "@/types";
+import { JSX } from "react";
 
-export const Statistics = () => {
+export const Statistics = (): JSX.Element => {
   return (
     <section id="statistics">
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
         {stats.map(({ quantity, description }: statsProps) => (
-          <div
-            key={description}
-            className="space-y-2 text-center"
-          >
+          <div key={description} className="space-y-2 text-center">
             <h2 className="text-3xl sm:text-4xl font-bold ">{quantity}</h2>
             <p className="text-xl text-muted-foreground">{description}</p>
           </div>
