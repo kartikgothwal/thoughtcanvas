9f7c531f84f7d5d986aa3ab1cf2c36f21d0830e0
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-04-19 23:48:24 +0530

    added the rate limiting function

--- lib/rateLimit.ts
+++ lib/rateLimit.ts
@@ -4,12 +4,16 @@ import { IRateLimit } from "@/types";
 const WINDOW_SIZE_IN_SECONDS = 60;
 const MAX_REQUESTS_PER_WINDOW = 10;
 
-export async function rateLimit({
-  identifier,
-  type,
-  maxRequests,
-  windowSeconds,
-}: IRateLimit) {
-  redis.set(`${identifier}:requests`,0)
+export async function rateLimit({ identifier, type }: IRateLimit) {
+  const requests: string | null = await redis.get(`${identifier}:requests`);
+  if (!requests) {
+    await redis.set(`${identifier}:requests`, 1, "EX", WINDOW_SIZE_IN_SECONDS);
+    return true;
+  }
+  const requestsCount:number = parseInt(requests, 10);
+  if(requestsCount > MAX_REQUESTS_PER_WINDOW) {
+    return false;
+  }
+  await redis.incr(`${identifier}:requests`);
   return true;
 }
